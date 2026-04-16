6b6abeaad0a27a5127039c92e1fc4f92e713044f
owenhaydenjoiner <77207654+owenhaydenjoiner@users.noreply.github.com>
2026-04-16 10:36:50 +0100

    fix spelling of Material

--- InducPY_materials.py
+++ InducPY_materials.py
@@ -1,11 +1,11 @@
 import InducPY
-from InducPY import Materiel
+from InducPY import Material
 
 #Steel
 
 
 #Inconel
-Inconel_718 = Materiel(1.002, 1.25e-6, 435, 8320)
+Inconel_718 = Material(1.002, 1.25e-6, 435, 8320)
 
 #Copper
-Copper_C1000 = Materiel(0.99999, 1.724e-8, 394, 8920)
+Copper_C1000 = Material(0.99999, 1.724e-8, 394, 8920)
--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from fontTools.ttx import process
 import InducPY,InducPY_materials
 
 
-Test_part = InducPY.Part(0.155, 0.05757, 0, InducPY_materiels.Inconel_718)
-Coil = InducPY.Coil(120, InducPY_materiels.Copper_C1000, 120)
+Test_part = InducPY.Part(0.155, 0.05757, 0, InducPY_materials.Inconel_718)
+Coil = InducPY.Coil(120, InducPY_materials.Copper_C1000, 120)
 Process = InducPY.Heating_process(Test_part,Coil,20,1200)
 print(Process.required_power())
